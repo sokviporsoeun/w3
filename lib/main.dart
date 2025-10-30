b3bb831b51602699173999eb8b442a6e869cccf3
vipor <sokvipor123@gmail.com>
2025-10-31 00:30:41 +0700

    update ex4

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@
 //         backgroundColor: const Color.fromARGB(255, 223, 109, 155),
 //         body: const Center(
 //           child: Text(
-//             'Hello my name is Vipor !',
+//             'Hello my name is Vipor!',
 //             textAlign: TextAlign.center,
 //             style: TextStyle(
 //               color: Color.fromARGB(255, 239, 238, 239),
@@ -63,6 +63,7 @@
 //                 fontSize: 30, // text size
 //                 fontWeight: FontWeight.bold,
 //                 letterSpacing: 1.5,
+//                 decoration: TextDecoration.none,
 //               ),
 //             ),
 //           ),
@@ -72,7 +73,7 @@
 //   );
 // }
 
-//Ex3//
+// Ex3//
 
 // import 'package:flutter/material.dart';
 
@@ -101,6 +102,7 @@
 //                     fontSize: 30,
 //                     fontWeight: FontWeight.bold,
 //                     letterSpacing: 1.5,
+//                     decoration: TextDecoration.none
 //                   ),
 //                 ),
 //               ),
@@ -122,6 +124,7 @@
 //                     fontSize: 30,
 //                     fontWeight: FontWeight.bold,
 //                     letterSpacing: 1.5,
+//                     decoration: TextDecoration.none
 //                   ),
 //                 ),
 //               ),
@@ -148,6 +151,7 @@
 //                     fontSize: 30,
 //                     fontWeight: FontWeight.bold,
 //                     letterSpacing: 1.5,
+//                     decoration: TextDecoration.none,
 //                   ),
 //                 ),
 //               ),
@@ -161,7 +165,66 @@
 
 //Ex4//
 
+// import 'package:flutter/material.dart';
 
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: Container(
+//         padding: const EdgeInsets.all(40),
+//         margin: const EdgeInsets.all(20),
+
+//         child: Column(
+//           children: [
+//             // Q3
+//             CustomCard(text: "OOP", color: Colors.blue[100]),
+//             CustomCard(text: "DART", color: Colors.blue[300]),
+//             CustomCard(text: "FLUTTER", color: Colors.blue[600]),
+//           ],
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+// //Q1:extract Container into a Stateless widget (CustomCard)
+// //Q2
+// class CustomCard extends StatelessWidget {
+//   //constructor with required text parameter and optional color parameter
+//   const CustomCard({
+//     super.key,
+//     required this.text,
+//     this.color = Colors.blue, //default value is Blue
+//   });
+
+// //parametr
+//   final String text;
+//   final Color? color;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       margin: const EdgeInsets.symmetric(vertical: 20),
+//       decoration: BoxDecoration(
+//         color: color,
+//         borderRadius: BorderRadius.circular(20),
+//       ),
+//       height: 100,
+//       child: Center(
+//         child: Text(
+//           text,
+//           style: const TextStyle(
+//             color: Colors.white,
+//             fontSize: 30,
+//             fontWeight: FontWeight.bold,
+//             letterSpacing: 1.5,
+//             decoration: TextDecoration.none,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 
 
