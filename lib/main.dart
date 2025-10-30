57d8f6da3df2642154b059728b3d04080d24f521
vipor <sokvipor123@gmail.com>
2025-10-31 00:06:36 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -32,45 +32,136 @@
 
 //Ex2//
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Container(
-        //for Outer container
-        padding: const EdgeInsets.all(50), //for inner spacing
-        margin: const EdgeInsets.all(40),  //for outer spacing
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: Container(
+//         //for Outer container
+//         padding: const EdgeInsets.all(50), //for inner spacing
+//         margin: const EdgeInsets.all(40),  //for outer spacing
 
-        //for Background and styling
-        decoration: BoxDecoration(
-          color: Colors.blue[300],
-          borderRadius: BorderRadius.circular(20), //for rounded corners
-        ),
+//         //for Background and styling
+//         decoration: BoxDecoration(
+//           color: Colors.blue[300],
+//           borderRadius: BorderRadius.circular(20), //for rounded corners
+//         ),
 
-        //for child widget
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.blue[600],
-            borderRadius: BorderRadius.circular(20),
-          ),
+//         //for child widget
+//         child: Container(
+//           decoration: BoxDecoration(
+//             color: Colors.blue[600],
+//             borderRadius: BorderRadius.circular(20),
+//           ),
 
-          child: const Center(
-            child: Text(
-              'CADT STUDENTS',
-              style: TextStyle(
-                color: Colors.white, // text color
-                fontSize: 30, // text size
-                fontWeight: FontWeight.bold,
-                letterSpacing: 1.5,
-              ),
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
-}
+//           child: const Center(
+//             child: Text(
+//               'CADT STUDENTS',
+//               style: TextStyle(
+//                 color: Colors.white, // text color
+//                 fontSize: 30, // text size
+//                 fontWeight: FontWeight.bold,
+//                 letterSpacing: 1.5,
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+//Ex3//
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: Container(
+//         padding: const EdgeInsets.all(40),
+//         margin: const EdgeInsets.all(20),
+
+//         child: Column(
+//           children: [
+//             //First Container (OOP)
+//             Container(
+//               margin: const EdgeInsets.symmetric(vertical: 20),
+//               decoration: BoxDecoration(
+//                 color: Colors.blue[100],
+//                 borderRadius: BorderRadius.circular(20),
+//               ),
+//               height: 100,
+//               child: const Center(
+//                 child: Text(
+//                   'OOP',
+//                   style: TextStyle(
+//                     color: Colors.white,
+//                     fontSize: 30,
+//                     fontWeight: FontWeight.bold,
+//                     letterSpacing: 1.5,
+//                   ),
+//                 ),
+//               ),
+//             ),
+
+//             //Second Container (DART)
+//             Container(
+//               margin: const EdgeInsets.symmetric(vertical: 20),
+//               decoration: BoxDecoration(
+//                 color: Colors.blue[300],
+//                 borderRadius: BorderRadius.circular(20),
+//               ),
+//               height: 100,
+//               child: const Center(
+//                 child: Text(
+//                   'DART',
+//                   style: TextStyle(
+//                     color: Colors.white,
+//                     fontSize: 30,
+//                     fontWeight: FontWeight.bold,
+//                     letterSpacing: 1.5,
+//                   ),
+//                 ),
+//               ),
+//             ),
+
+//             //Third Container FLUTTER with gradient bonus
+//             Container(
+//               margin: const EdgeInsets.symmetric(vertical: 20),
+//               decoration: BoxDecoration(
+//                 gradient: LinearGradient(
+//                   colors: [Colors.blue[300]!, Colors.blue[600]!],
+//                   begin: Alignment.topLeft,
+//                   end: Alignment.bottomRight,
+//                 ),
+
+//                 borderRadius: BorderRadius.circular(20),
+//               ),
+//               height: 100,
+//               child: const Center(
+//                 child: Text(
+//                   'FLUTTER',
+//                   style: TextStyle(
+//                     color: Colors.white,
+//                     fontSize: 30,
+//                     fontWeight: FontWeight.bold,
+//                     letterSpacing: 1.5,
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+//Ex4//
+
+
 
 
 
